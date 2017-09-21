81a0385eb81d7f58496d8199de9c526332b83594
Andrew <andrew.bollinger@empa.ch>
2017-09-21 17:40:01 +0200

    first commit

---
+++ equations_library.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 16:46:59 2017
+
+@author: boa
+"""
+
+class EquationsLibrary:
+    
+    def __init__(self):
+        
+    def energy_balance_constraint(self):
+        
+    def conversion_technology_capacity_constraint(self):
+        
+    def storage_technology_capacity_constraint(self):
+        
+    def storage_technology_continuity_constraint(self):
+    
+        
+        
